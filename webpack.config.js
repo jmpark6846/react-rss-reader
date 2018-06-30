41dcc5f0579fd42f2c0133c44ca156cea5b58e73
jmpark6846 <jmpark6846@gmail.com>
2018-06-30 09:23:26 +0900

    webpack 개발설정

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode:'development',
+  entry:{
+    // vendor: [''],
+    app: './src/index.js'
+  },
+  output:{
+    filename: '[name].[hash].js',
+  },
+  module:{
+    rules:[
+      {
+        test:/\.js$/,
+        exclude: /node_modules/,
+        use:['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', 
+          { 
+            loader: 'css-loader', 
+            options: {
+              modules: true,
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+  devServer: {
+    host: 'localhost',
+    port: process.env.PORT || 3000,
+    historyApiFallback: true,
+    open: true,
+    hot: true,
+  }
+}
